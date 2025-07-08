9dbba7aae68242cb46381ddf223b034d0d67b7d2
Arpit <omagrahari55@gmail.com>
2025-07-08 12:25:59 +0530

    section 5 of homepage is done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Section2 from '@/components/Section2'
 import Section3 from '@/components/Section3'
 import React from 'react'
 import Section4 from '../components/Section4'
+import Section5 from '@/components/Section5'
 
 const page = () => {
   return (
@@ -13,6 +14,7 @@ const page = () => {
     <Section2/>
     <Section3/>
     <Section4/>
+    <Section5/>
     </>
   )
 }
--- src/components/Section4.tsx
+++ src/components/Section4.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
 import Link from 'next/link';
+import Sec4Image1 from "@/assets/Sec4Image1.avif"
+import Image from 'next/image';
+
 
 function createData(
     Feature: string,
@@ -33,30 +34,37 @@ const Section4 = () => {
     <>
         <section className='pb-[4rem] ' >
             <h2 className='text-center bungee-font text-[3rem] font-semibold ' >Compare Plans</h2>
-            <div className='flex justify-centet items-center mt-[3rem]   ' >
-              <Table className='!w-[50%] m-auto bg-[#00000008] rounded-[1rem] !shadow-[0px_4px_19px_8px_#4042434d]   '   aria-label="simple table">
-                      <TableHead>
-                      <TableRow>
-                          <TableCell className='!text-[1.5rem] !text-blue-600 !text-center !font-bold ' >Feature</TableCell>
-                          <TableCell className='!text-[1.5rem] !text-blue-600 !text-center !font-bold '  >Free</TableCell>
-                          <TableCell className='!text-[1.5rem] !text-blue-600 !text-center !font-bold ' >Pro</TableCell>
-                          <TableCell className='!text-[1.5rem] !text-blue-600 !text-center !font-bold ' >Starter</TableCell>
-                      </TableRow>
-                      </TableHead>
-                      <TableBody>
-                      {rows.map((row,index) => (
-                          <TableRow key={index}>
-                          <TableCell className='!text-[1.2rem] !text-gray-700 !text-center !font-semibold ' >{row.Feature}</TableCell>
-                          <TableCell className='!text-[1.2rem] !text-gray-700 !text-center !font-semibold ' >{row.Free}</TableCell>
-                          <TableCell  className='!text-[1.2rem] !text-gray-700 !text-center !font-semibold ' >{row.Starter}</TableCell>
-                          <TableCell  className='!text-[1.2rem] !text-gray-700 !text-center !font-semibold ' >{row.Pro}</TableCell>
+            <div className='w-full flex justify-center items-center mt-[5rem] ' >
+              <div className='w-[55%]' >
+                <div className='flex justify-centet items-center   ' >
+                  <Table className='!w-[90%] m-auto bg-[#00000008] rounded-[1rem] !shadow-[0px_4px_19px_8px_#4042434d]   '   aria-label="simple table">
+                          <TableHead>
+                          <TableRow>
+                              <TableCell className='!text-[1.5rem] !text-blue-600 !text-center !font-bold ' >Feature</TableCell>
+                              <TableCell className='!text-[1.5rem] !text-blue-600 !text-center !font-bold '  >Free</TableCell>
+                              <TableCell className='!text-[1.5rem] !text-blue-600 !text-center !font-bold ' >Pro</TableCell>
+                              <TableCell className='!text-[1.5rem] !text-blue-600 !text-center !font-bold ' >Starter</TableCell>
                           </TableRow>
-                      ))}
-                      </TableBody>
-              </Table>
-            </div>
-            <div className='flex justify-center items-center mt-[3rem] '   >
-              <Link href={"/pricing"} className='px-[5rem]  py-[0.7rem] bg-blue-600 shadow-[0px_4px_19px_8px_#4042434d] hover:scale-[1.03] transition-all rounded-[1rem] text-[2rem] text-white font-semibold  ' >View full pricing →</Link>
+                          </TableHead>
+                          <TableBody>
+                          {rows.map((row,index) => (
+                              <TableRow key={index}>
+                              <TableCell className='!text-[1.2rem] !text-gray-700 !text-center !font-semibold ' >{row.Feature}</TableCell>
+                              <TableCell className='!text-[1.2rem] !text-gray-700 !text-center !font-semibold ' >{row.Free}</TableCell>
+                              <TableCell  className='!text-[1.2rem] !text-gray-700 !text-center !font-semibold ' >{row.Starter}</TableCell>
+                              <TableCell  className='!text-[1.2rem] !text-gray-700 !text-center !font-semibold ' >{row.Pro}</TableCell>
+                              </TableRow>
+                          ))}
+                          </TableBody>
+                  </Table>
+                </div>
+                <div className='flex justify-center items-center mt-[3rem] '   >
+                  <Link href={"/pricing"} className='px-[5rem]  py-[0.7rem] bg-blue-600 shadow-[0px_4px_19px_8px_#4042434d] hover:scale-[1.03] transition-all rounded-[1rem] text-[2rem] text-white font-semibold  ' >View full pricing →</Link>
+                </div>
+              </div>
+              <div className='w-[40%]' >
+                  <Image src={Sec4Image1} alt='' />
+              </div>
             </div>
         </section>
     </>
